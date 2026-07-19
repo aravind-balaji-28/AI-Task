e39e77a6ed9a9b74de12fc9489f1798547ced142
Aravind <balajiaravind@gmail.com>
2026-07-20 00:53:16 +0530

    update component based code splitting

--- Ai-Frontend/src/components/Dashboard.tsx
+++ Ai-Frontend/src/components/Dashboard.tsx
@@ -1,5 +1,24 @@
+import React, { Suspense, useState } from "react";
+
+const UserInfo = React.lazy(() => import("./UserInfo"))
+
 function Dashboard() {
-  return <h1>Welcome to Dashboard</h1>;
+  const [isOpen, setIsOpen] = useState(false)
+  return <>
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      <h1>Welcome to Dashboard</h1>
+      <button style={{ width: "10rem" }} onClick={() => {
+        setIsOpen(p => !p)
+      }}>View Profile</button>
+
+      {isOpen && (
+        <Suspense fallback={<p>Loading...</p>}>
+          <UserInfo />
+        </Suspense>
+
+      )}
+    </div>
+  </>;
 }
 
 export default Dashboard;
---
+++ Ai-Frontend/src/components/UserInfo.tsx
@@ -0,0 +1,11 @@
+function UserInfo() {
+  return <>
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      <div>Name: John</div>
+      <div>Age: 25</div>
+      <div>Role: Developer</div>
+    </div>
+  </>
+}
+
+export default UserInfo;
